b0216a2759033b6f950a25307aff58c6498ea0f9
Navid Constantinou <navidcy@gmail.com>
2020-06-17 09:41:55 +1000

    reproducing figure 1 by Tzou et al 2013

---
+++ examples/README.md
@@ -0,0 +1,15 @@
+# Brusselator.jl/examples
+
+These are some basic examples of the various modules included in Brusselator.jl. 
+
+## Run the examples
+
+Alternatively, you can run these scripts directly. While inside the `examples` folder do
+```julia
+] instantiate
+```
+to install dependencies. Then run any of the examples by
+```julia
+include("example_script.jl")
+```
+
--- examples/example_script_Tzou.jl
+++ examples/example_script_Tzou.jl
@@ -1,6 +1,6 @@
 using Brusselator 
 
-using Plots
+using Plots, Printf
 
 using FFTW: irfft
 
@@ -16,10 +16,11 @@ nothing # hide
 
 # ## Numerical parameters and time-stepping parameters
 
-     nx = 256            # grid resolution
-stepper = "ETDRK4"      # timestepper
-     dt = 0.005           # timestep
- nsteps = 5000           # total number of time-steps
+     nx = 512            # grid resolution
+stepper = "RK4"          # timestepper
+     dt = 0.01           # timestep
+ nsteps = 2500           # total number of time-steps
+ nsubs  = 25             # number of time-steps for intermediate logging/plotting (nsteps must be multiple of nsubs)
 nothing # hide
 
 
@@ -28,6 +29,8 @@ nothing # hide
 E = 1.4
 
 L = 137.37     # Domain length
+
+# Parameters used by Tzou et al. (2013)
 ε = 0.1
 μ = 25
 ρ = 0.178
@@ -38,7 +41,7 @@ B_H = (1 + E * sqrt(D_c))^2
 B = B_H + ε^2 * μ
 D = D_c + ε^2 * ρ
    
-kc = sqrt(E/sqrt(D))
+kc = sqrt(E / sqrt(D))
 
 nothing # hide
 
@@ -59,68 +62,12 @@ nothing #hide
 get_u(prob) = irfft(prob.sol[:, 1], prob.grid.nx)
 get_v(prob) = irfft(prob.sol[:, 2], prob.grid.nx)
 
-u = Diagnostic(get_u, prob; nsteps=nsteps)
-v = Diagnostic(get_v, prob; nsteps=nsteps)
-diags = [u]
-# ## Setting initial conditions
-
-# For initial condition we take the fluid at rest ($u=v=0$). The free surface elevation
-# is perturbed from its rest position ($\eta=0$); the disturbance we impose a Gaussian 
-# bump with half-width greater than the deformation radius and on top of that we 
-# superimpose some random noise with scales smaller than the deformation radius. 
-# We mask the small-scale perturbations so that it only applies in the central part 
-# of the domain by applying
-#
-# The system develops geostrophically-balanced jets around the Gaussian bump, 
-# while the smaller-scale noise propagates away as inertia-gravity waves. 
-
-# First let's construct the Gaussian bump.
-# Next the noisy perturbation. The `mask` is simply a product of $\tanh$ functions.
-
-l = 28;
-θ = @. (grid.x > -l/2) & (grid.x < l/2)
-
-
-au, av = -(E^2 + kc^2) / B, -1
-cu, cv = -E * (E + im) / B, 1
-
-u0 = @.  E  + ε * real( au * exp(im * kc * grid.x) * θ + cu * (1-θ) )
-v0 = @. B/E + ε * real( av * exp(im * kc * grid.x) * θ + cv * (1-θ) )
-
-# plot_u0 = plot(grid.x, u0,
-#                color = :black,
-#               legend = :false,
-#            linewidth = [3 2],
-#                alpha = 0.7,
-#                xlims = (-L/2, L/2),
-#                # ylims = (-0.3, 0.3),
-#               xlabel = "x",
-#               ylabel = "u(x, 0)")
-# 
-# plot_v0 = plot(grid.x, v0,
-#                color = :black,
-#               legend = :false,
-#            linewidth = [3 2],
-#                alpha = 0.7,
-#                xlims = (-L/2, L/2),
-#                # ylims = (-0.3, 0.3),
-#               xlabel = "x",
-#               ylabel = "v(x, 0)")
-# 
-# 
-# title = plot(title = "initial conditions", grid = false, showaxis = false, bottom_margin = -20Plots.px)
-# 
-# plot(title, plot_u0, plot_v0,
-#            layout = @layout([A{0.01h}; [B; C]]),
-#              size = (600, 400))
-
-# Sum the Gaussian bump and the noise and then call `set_uvη!()` to set the initial condition to the problem `prob`.
-
+u_solution = Diagnostic(get_u, prob; nsteps=nsteps+1)
+diags = [u_solution]
 
 # ## Visualizing the simulation
 
-# We define a function that plots the surface elevation $\eta$ and the 
-# depth-integrated velocities $u$ and $v$. 
+# We define a function that plots $u$ and $v$. 
 
 function plot_output(prob)
   plot_u = plot(grid.x, vars.u,
@@ -129,7 +76,7 @@ function plot_output(prob)
              linewidth = 2,
                  alpha = 0.7,
                  xlims = (-L/2, L/2),
-                 # ylims = (-0.3, 0.3),
+                 ylims = (E-1.5, E+1.5),
                 xlabel = "x",
                 ylabel = "u")
 
@@ -139,22 +86,37 @@ function plot_output(prob)
              linewidth = 2,
                  alpha = 0.7,
                  xlims = (-L/2, L/2),
-                 # ylims = (-0.3, 0.3),
+                 ylims = (B/E-1.5, B/E+1.5),
                 xlabel = "x",
                 ylabel = "v")
-
   
   title = plot(title = "Brusselator", grid = false, showaxis = false, bottom_margin = -20Plots.px)
   
   return plot(title, plot_u, plot_v, 
            layout = @layout([A{0.01h}; [B; C]]),
-             size = (600, 400))
+             size = (600, 400),
+              dpi = 150)
 end
 nothing # hide
 
+# ## Setting initial conditions
 
+# We reproduce here (to our best knowledge possible) the initial condition used
+# by Tzou et al. (2013).
+
+l = 28;
+θ = @. (grid.x > -l/2) & (grid.x < l/2)
+
+au, av = -(E^2 + kc^2) / B, -1
+cu, cv = -E * (E + im) / B, 1
+
+u0 = @.  E  + ε * real( au * exp(im * kc * grid.x) * θ + cu * (1 - θ) )
+v0 = @. B/E + ε * real( av * exp(im * kc * grid.x) * θ + cv * (1 - θ) )
 
 set_uv!(prob, u0, v0)
+
+# Let's plot the initial condition.
+
 plot_output(prob)
 
 
@@ -163,24 +125,35 @@ plot_output(prob)
 # We time-step the `Problem` forward in time. We update variables by calling 
 # `updatevars!()` and we also update the plot. We enclose the `for` loop in 
 # an `@animate` macro to produce an animation of the solution.
- 
-# 
-stepforward!(prob, diags, nsteps)
-U = zeros(grid.nx, nsteps+1)
-[ U[:, j] = u.data[j] for j=1:nsteps+1 ]
 
-heatmap(grid.x, u.t, U', c = :grayC, clims=(1, 3))
+startwalltime = time()
 
-# p = plot_output(prob)
-# 
-# anim = @animate for j=0:nsteps
-#   updatevars!(prob)
-# 
-#   p[2][1][:y] = vars.u    # updates the plot for u
-#   p[2][:title] = "t = "*@sprintf("%.1f", prob.clock.t) # updates time in the title
-#   p[3][1][:y] = vars.v    # updates the plot for v
-# 
-#   stepforward!(prob)
-# end
-# 
-# mp4(anim, "brusselator.mp4", fps=18)
+p = plot_output(prob)
+
+anim = @animate for j=0:Int(nsteps/nsubs)
+  
+  log = @sprintf("step: %04d, t: %.1f, walltime: %.2f min",
+  prob.clock.step, prob.clock.t, (time()-startwalltime)/60)
+
+  if j%(500/nsubs)==0; println(log) end
+  
+  updatevars!(prob)
+
+  p[2][1][:y] = vars.u    # updates the plot for u
+  p[2][:title] = "t = "*@sprintf("%.1f", prob.clock.t) # updates time in the title
+  p[3][1][:y] = vars.v    # updates the plot for v
+
+  stepforward!(prob, diags, nsubs)
+end
+
+mp4(anim, "brusselator.mp4", fps=18)
+
+# Now let's plot a space-time diagram of the $u$ solution.
+
+t = u_solution.t[1:nsteps+1]
+U_xt = zeros(grid.nx, nsteps+1)
+[ U_xt[:, j] = u_solution.data[j] for j=1:nsteps+1 ]
+
+heatmap(grid.x, t, U_xt',
+             c = :grayC,
+         clims = (1, 3))
